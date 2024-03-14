d3fcaf6f22c929f731353021df14ae7edf7894d7
irishcarbomb777 <danielclannan@gmail.com>
2024-03-14 11:12:09 -0400

    mod

--- src/types/AirTree.ts
+++ src/types/AirTree.ts
@@ -10,22 +10,28 @@ import { HasTail, Head, Tail } from "./utlity"
 const emptySymbol = Symbol('EmptyObject type')
 type EmptyObject = {[emptySymbol]?: never}
 
-export type AirTree<T extends AirNode<any, any>[]> = Head<T> 
-    & (HasTail<T> extends true 
-        ?  { 
-            children: Map<string, AirTree<Tail<T>>>
-        }
-        : object
-    )
+// export type AirTree<T extends AirNode<any, any>[]> = Head<T> 
+//     & (HasTail<T> extends true 
+//         ?  { 
+//             children: Map<string, AirTree<Tail<T>>>
+//         }
+//         : object
+//     )
+
+export type AirTree<T extends AirNode<any, any>[]> = HasTail<T> extends true 
+    ? Head<T> & {
+        children: Map<string, AirTree<Tail<T>>>
+    }
+    : Head<T>
 
 
-type Thing = {
-    name: string
-}
+// type Thing = {
+//     name: string
+// }
 
-type Thing2 = Thing & EmptyObject
+// type Thing2 = Thing & EmptyObject
 
-const obj: Thing2 = {
-    name: "hello",
-}
+// const obj: Thing2 = {
+//     name: "hello",
+// }
 
