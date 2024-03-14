7b172709273edd66817ece3079c4f2cfe2d8a57e
irishcarbomb777 <danielclannan@gmail.com>
2024-03-14 10:30:07 -0400

    mod

--- src/types/AirTree.ts
+++ src/types/AirTree.ts
@@ -1,8 +1,18 @@
 import { AirNode } from "./AirNode"
 import { HasTail, Head, Tail } from "./utlity"
 
-export type AirTree<T extends AirNode<any, any>[]> = Head<T> & { 
-    children: HasTail<T> extends true 
-        ? Map<string, AirTree<Tail<T>>>
-        : Partial<never>
-}
+// export type AirTree<T extends AirNode<any, any>[]> = Head<T> & { 
+//     children: HasTail<T> extends true 
+//         ? Map<string, AirTree<Tail<T>>>
+//         : Partial<never>
+// }
+
+export type AirTree<T extends AirNode<any, any>[]> = Head<T> 
+    & HasTail<T> extends true 
+        ? & { 
+            children: Map<string, AirTree<Tail<T>>>
+        }
+        : | never
+
+
+// type Thing2 = Thing | never
